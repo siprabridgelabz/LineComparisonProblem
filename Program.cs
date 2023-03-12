42015c1c1e8e4972800f215e1220156844d3a73c
sipra <sipramishra085@gmail.com>
2023-03-12 21:47:50 +0530

    Added file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LineComparisonProblem;
+
+namespace LineComparisionComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+            //LineDifference.LineDistance();
+           // CheckLineEquality.LineEquality();
+            CompareTwoLines.CompareLines();
+        }
+    }
+}
