e1676fe0c9d101e3c8089e138cc58d56471a8c2b
sipra <sipramishra085@gmail.com>
2023-03-12 21:52:35 +0530

    Added UC1

---
+++ LineDifference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineDifference
+    {
+        public static void LineDistance()
+        {
+            Console.WriteLine("Enter the value of x-cordinate of point1: x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y-cordinate of point1: y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x-cordinate of point2: x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y-cordinate of point1: y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("Distance between two line " + "(" + x1 + ", " + y1 + ")" + "(" + x2 + ", " + y2 + ") is: " + length);
+        }
+    }
+}
